005a3076c5233322daa4b7ec3df134950676aa97
drolegc <drolegc@penguin>
2019-10-20 14:02:58 -0300

    Testing serializer - generic View

--- a_pedal/users/urls.py
+++ a_pedal/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from users.views import PerfilTest
+
 urlpatterns = [
-    
+    path('perfiles/',PerfilTest.as_view(),name='perfiles'),
 ]
--- a_pedal/users/views.py
+++ a_pedal/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+#Models
+
+from users.models import Perfil
+from django.contrib.auth.models import User
+
+#Serializers
+from users.serializers import PerfilSerializer
+
+#REST
+from rest_framework import generics
+#from rest_framework.permissions import IsAdminUser
+
 # Create your views here.
+
+class PerfilTest(generics.ListCreateAPIView):
+    queryset = Perfil.objects.all()
+    serializer_class = PerfilSerializer
